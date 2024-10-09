22d3067a480a8fa6fea2b4340cb6fe9662fd8cd1
gui2310g <fontesguilherme57@gmail.com>
2024-10-08 23:17:19 -0300

    added game component && filter button in gamespage feature

---
+++ src/app/components/game/game.component.ts
@@ -0,0 +1,51 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgStyle, NgOptimizedImage } from '@angular/common';
+
+export interface Game {
+  id: number;
+  image: string;
+  alt: string;
+}
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [NgStyle, NgOptimizedImage],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss',
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class GameComponent {
+  games: Game[] = [
+    {
+      id: 1,
+      image: 'https://cdn2.steamgriddb.com/grid/f570bd23dab3563703638856f9d0952d.png',
+      alt: 'crash mind over mutant',
+    },
+    {
+      id: 2,
+      image: 'https://cdn2.steamgriddb.com/grid/4f02cebf5673725a4ef1b1f2f9a95080.png',
+      alt: 'obscure image',
+    },
+    {
+      id: 3,
+      image: 'https://cdn2.steamgriddb.com/grid/a9f9ef04f6c8bcc449084e0cc4183cf7.png',
+      alt: 'red dead redemption logo',
+    },
+    {
+      id: 4,
+      image: 'https://cdn.cloudflare.steamstatic.com/steam/apps/42680/library_600x900_2x.jpg?t=1566872035',
+      alt: 'mw3 logo',
+    },
+    {
+      id: 5,
+      image: 'https://cdn2.steamgriddb.com/grid/4f70afdc96d8fdd57e8f69f0759b01e7.png',
+      alt: 'borderlands',
+    },
+    {
+      id: 6,
+      image: 'https://cdn2.steamgriddb.com/grid/ca70ee660dbe041ce148b902503fd5a0.png',
+      alt: ' bf4 logo',
+    }
+  ];
+}
--- src/app/features/gamespage/gamespage.component.ts
+++ src/app/features/gamespage/gamespage.component.ts
@@ -1,12 +1,14 @@
 import { Component } from "@angular/core";
 import { GamesListsComponent } from "../../components/gameslists/gameslists.component";
-import { MatDivider } from "@angular/material/divider";
+import { GameComponent } from "../../components/game/game.component";
+import { MatIcon } from "@angular/material/icon";
+import { MatButton } from "@angular/material/button";
 @Component({
     selector: "app-gamepage",
     templateUrl: "./gamespage.component.html",
     styleUrl: "./gamespage.component.scss",
     standalone: true,
-    imports: [GamesListsComponent, MatDivider]
+    imports: [GamesListsComponent, MatIcon, MatButton, GameComponent]
 })
 export class GamesPageComponent {
 }
