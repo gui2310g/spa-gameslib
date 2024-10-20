f9614874ecbc537f63d9553770b61867c939f1a7
gui2310g <fontesguilherme57@gmail.com>
2024-10-20 00:11:16 -0300

    added paginator in game component

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NgStyle, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { GameService } from '../../services/gameservice/gameservice.service';
+import { PaginatorModule } from 'primeng/paginator';
 
-export interface Game {
+interface Game {
   id: number;
   slug: string;
   name: string
@@ -12,30 +13,41 @@ export interface Game {
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [NgStyle, NgOptimizedImage, RouterLink],
+  imports: [NgStyle, NgOptimizedImage, RouterLink, PaginatorModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss',
   encapsulation: ViewEncapsulation.Emulated
 })
 export class GameComponent implements OnInit{
   games: Game[] = [];
-    page: number = 0;
-    size: number = 10;
+  page: number = 0;
+  size: number = 6;
+  totalRecords: number = 0;
+  first: number = 0;
 
-    constructor(private gameService: GameService) {}
+  constructor(private gameService: GameService) {}
 
-    ngOnInit(): void {
-        this.loadGames();
-    }
+  ngOnInit(): void {
+    this.loadGames();
+  }
 
-    loadGames(): void {
-        this.gameService.getGames(this.page, this.size).subscribe({
-            next: (data) => this.games = data.content,
-            error: (error) => console.error("Error by finding games", error),
-            complete: () => console.log("Games loaded successfully")
-        });
-    }
+  loadGames(): void {
+    this.gameService.getGames(this.page, this.size).subscribe({
+      next: (data) => {
+        this.games = data.content,
+        this.totalRecords = data.totalElements;
+      },
+      error: (error) => console.error("Error by finding games", error),
+        complete: () => console.log("Games loaded successfully")
+      }
+    );
+  }
 
+  onPageChange(event: any): void {
+    this.page = event.page; 
+    this.first = event.first; 
+    this.loadGames(); 
+  }
 }
 
 
