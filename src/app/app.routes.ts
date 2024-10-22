d83b1c6440ffe617f9cbc19f05ba9cd8e3ff75f1
gui2310g <fontesguilherme57@gmail.com>
2024-10-22 15:03:53 -0300

    created homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { GamePageComponent } from './pages/gamepage/gamepage.component';
 import { GamesSearchComponent } from './pages/gamesSearch/gamesSearch.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: GamesSearchComponent,
+    component: HomepageComponent
+  },
+  {
+    path: 'search',
+    component: GamesSearchComponent
   },
   {
     path: 'game',
-    component: GamePageComponent,
+    component: GamePageComponent
   },
 ];
