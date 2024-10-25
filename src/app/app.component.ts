7b9eed79a1d6c60cb0a0beef855586dda0b19781
gui2310g <fontesguilherme57@gmail.com>
2024-10-24 21:13:14 -0300

    added titles-section and description-section in homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { GamesSearchComponent } from './pages/gamesSearch/gamesSearch.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, GamesSearchComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatIcon } from '@angular/material/icon';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, NgClass],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-
+  reverse: string = 'reverse'
 }
