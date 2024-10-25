c5482ab8d854ef0b4614569434adec10eb41606c
gui2310g <fontesguilherme57@gmail.com>
2024-10-25 00:31:17 -0300

    transferred descriptions-section in homepage to gameslists component

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { GamesListsComponent } from '../../components/gameslists/gameslists.component';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, GamesListsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
