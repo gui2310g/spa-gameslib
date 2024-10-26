2c2a21a97faf0ac02566b20312be9e9d40272009
gui2310g <fontesguilherme57@gmail.com>
2024-10-25 23:53:51 -0300

    redone gameslists component with @input, media queries and ngClass

--- src/app/components/gameslists/gameslists.component.ts
+++ src/app/components/gameslists/gameslists.component.ts
@@ -1,13 +1,17 @@
-import { Component } from "@angular/core";
-
-
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 @Component({
-    selector: "app-gameslists",
-    templateUrl: "./gameslists.component.html",
-    styleUrl: "./gameslists.component.scss",
-    standalone: true,
-    imports: [] 
-})  
-export class GamesListsComponent  {
+  selector: 'app-gameslists',
+  templateUrl: './gameslists.component.html',
+  styleUrl: './gameslists.component.scss',
+  standalone: true,
+  imports: [NgClass],
+})
 
+export class GamesListsComponent{
+    @Input() title: string = '';  
+    @Input() description: string = ''; 
+    @Input() src: string = '';  
+    @Input() alt: string = ''; 
+    @Input() isReverse: boolean = true;
 }
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { GamesListsComponent } from '../../components/gameslists/gameslists.component';
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [MatIcon, GamesListsComponent],
+  imports: [MatIcon, GamesListsComponent, NgClass],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
