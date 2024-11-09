38b43fcaaa7cdf4a4d0d32925b758c07c6d80f10
gui2310g <fontesguilherme57@gmail.com>
2024-11-09 12:27:15 -0300

    added createUser and HandleError in userservice, added onsubmit in signup page and created user model'

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/userservice/usersservice.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.scss'
+  styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      name: ['', Validators.required], 
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      const newUser: User = this.signupForm.value;
+      this.userService.createUser(newUser).subscribe({
+        next: () => {
+          window.alert('User created successfully!');
+          this.router.navigate(['']);
+        },
+        error: (error) => { 
+          window.alert(error.message); 
+        }
+      });
+    } else {
+      window.alert('Please fill out the form correctly.');
+    }
+  }
+  
 }
--- src/app/services/userservice/usersservice.service.ts
+++ src/app/services/userservice/usersservice.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../../environments/environment.development';
+import { User } from '../../models/user.model';
 
-@Injectable({ providedIn: 'root' })
-
+@Injectable({
+  providedIn: 'root',
+})
 export class UserService {
-  private apiUrl = process.env["API_URL"];
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
-  
+  createUser(user: User): Observable<any> {
+    return this.http
+      .post<any>(`${this.apiUrl}/users/create`, user)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'An error occurred. Please try again.';
+    if (error.status === 409) errorMessage = 'User or email already exists';
+    
+    return throwError(() => new Error(errorMessage));
+  }
 }
